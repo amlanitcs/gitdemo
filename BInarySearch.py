7c609ab3340cc4e4faffaff079531449e68dc479
amlanitcs <amlan.itcs@gmail.com>
2024-07-03 23:58:15 +0530

    Completed Code

---
+++ BInarySearch.py
@@ -0,0 +1,45 @@
+import math
+class BinarySearch:
+
+    def __init__(self, AList):
+        self.alist=AList
+        AList.sort(reverse=False)
+        print(self.alist)
+
+
+    def BS(self,NumberToFind, ListofNumberstoPass):
+        firstIndex=0
+        midIndex =0
+        lastIndex= len(ListofNumberstoPass)-1
+        found= 'false'
+
+        while firstIndex<=lastIndex:
+            midIndex =((firstIndex+lastIndex) // 2)
+            if NumberToFind> int(ListofNumberstoPass[midIndex]):
+                firstIndex=midIndex+1
+            elif NumberToFind< int(ListofNumberstoPass[midIndex]):
+                lastIndex=midIndex-1
+            elif NumberToFind==int(ListofNumberstoPass[midIndex]):
+                print(" midPos="+ str(midIndex) + " firstPos="+str(firstIndex)+ " lastPos=" + str(lastIndex))
+                found='true'
+                break
+
+        if found=='false':
+            print("not Found")
+
+
+
+
+
+
+print("enter numbers with space inbetween")
+Li = str(input()).split(" ")
+print(Li)
+
+B= BinarySearch(Li)
+print("enter the number to Search")
+N=int(input())
+B.BS(N,Li)
+
+
+
